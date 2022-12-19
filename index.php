912916ffa5e6c1e24fbb399ff8936b20747e40b5
Charlie Simpson <charliesimpson@Charlies-Air.hub>
2022-12-19 14:45:59 +1100

    I SWEAR PLEASE PLEASEE PLSEASE

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Import the Composer Autoloader to make the SDK classes accessible:
-require '../vendor/autoload.php';
+require 'vendor/autoload.php';
 
 // Load our environment variables from the .env file:
 (Dotenv\Dotenv::createImmutable(__DIR__))->load();
